69f61f997b869054df17bb2e2fc1123343f4f552
𝕾𝖆𝖒𝖒𝖎𝖊𝖑 <namuel181@gmail.com>
2026-06-06 12:24:45 +0000

    revert: settings.js to zip base

--- src/settings.js
+++ src/settings.js
@@ -14,7 +14,7 @@ const DEFAULTS = {
   antidelete: false,
   autoblock: false,
   gconly: false,
-  menuBgUrl: "https://litter.catbox.moe/86d9vf.jpg",
+  menuBgUrl: "",  // leave empty to use the bundled local image (src/assets/menu_bg.jpg)
   channelId: "120363406397452589@newsletter",
   channelName: "Yuzuki   更新",
   owners: [],
